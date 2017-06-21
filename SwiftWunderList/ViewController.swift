e120d6ec2b1c475c0f1bea13727da7b05f8c2e0f
kinjoyuji <kinjoyuji@gmail.com>
2017-06-22 04:37:33 +0900

    アプリ内に保存したデータを取り出して表示

--- SwiftWunderList/ViewController.swift
+++ SwiftWunderList/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var textField: UITextField!
     
+    var titleArray = [String]()
+    
+    var label:UILabel = UILabel()
     
     
     
@@ -39,12 +42,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    // 画面が表示される都度呼び出される
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // アプリに保存されているToDoを取得する
+        if UserDefaults.standard.object(forKey: "titleArray") != nil {
+            titleArray = UserDefaults.standard.object(forKey: "titleArray") as! [String]
+        }
+        
+        //tableViewをreloadする
+        
+        table.reloadData()
+    }
+    
     
     // textFieldのデリゲートメソッド
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        // returnキーが押されるとキーボードを閉じる
-        textField.resignFirstResponder()
+        
+        // titleArrayに文字列を保存する
+        titleArray.insert(textField.text!, at: 0)
+        UserDefaults.standard.set(titleArray, forKey: "titleArray")
+        
+        if UserDefaults.standard.object(forKey: "titleArray") != nil {
+            titleArray = UserDefaults.standard.object(forKey: "titleArray") as! [String]
+        
+            // returnキーが押されるとキーボードを閉じる
+            textField.resignFirstResponder()
+            
+            textField.text = ""
+            
+            // tableViewをreloadする
+            table.reloadData()
+        
+        }
         
         return true
     
@@ -62,14 +94,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //セクション数を決める
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
     
     
     // セルの数を決める
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return titleArray.count
     }
     
     
@@ -77,9 +109,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        // Configure the cell...
-        
         cell.layer.cornerRadius = 10.0
+    
+        label = cell.contentView.viewWithTag(1) as! UILabel
+        
+        label.text = titleArray[indexPath.row]
         
         return cell
     }

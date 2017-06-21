5b69b2b1f18c15b04937f5088cd550fe0b080ae5
kinjoyuji <kinjoyuji@gmail.com>
2017-06-22 01:40:56 +0900

    ViewControllerの設定

--- SwiftWunderList/ViewController.swift
+++ SwiftWunderList/ViewController.swift
@@ -8,16 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+    @IBOutlet weak var backView: UIView!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    
+    // 初回ロードされた時のみ呼び出される
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //読み込み時に, tableView, textFieldをデリゲートする
+        backView.layer.cornerRadius = 10.0
+        table.delegate = self
+        table.dataSource = self
+        textField.delegate = self
+        
+        
+        // cellの下のborderLineが不必要
+        table.separatorStyle = .none
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+    // tableViewControllerのデリゲートメソッド. これがないとエラーになる. TableViewControllerを作ると初期状態で
+    // 作られるメソッドたち.↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    
+    //セクション数を決める
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    
+    // セルの数を決める
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    
+    // セルの状態.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // Configure the cell...
+        
+        cell.layer.cornerRadius = 10.0
+        
+        return cell
     }
 
 
